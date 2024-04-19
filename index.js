1c4ed85a10b9cec37e5f52c4be0bad62a0f31554
YeetDragon23 <100045569+yeetdragon24@users.noreply.github.com>
2024-04-18 20:07:43 -0400

    fixed an issue

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+if (Math.random() < 0.5) vid.src='https://www.youtube.com/embed/EyOnl8oRDdA?autoplay=1';
 function setSize() {
     let vid = document.getElementById('video');
     let w = window.innerWidth, h = window.innerHeight;
-    if (true) vid.src='https://www.youtube.com/embed/EyOnl8oRDdA?autoplay=1';
     vid.width=w*1.25;
     vid.height=h*1.25;
     document.body.style.margin=`-${h/10}px 0 0 -${(w/10)-(w/50)}px`;
